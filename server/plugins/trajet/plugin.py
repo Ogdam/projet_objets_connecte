0bfa4da8ca5e6fbd4372195685feaf13f470b2a1
Fab <venier.fabien@outlook.fr>
2021-02-24 12:21:20 +0100

    fix 3

--- server/plugins/trajet/plugin.py
+++ server/plugins/trajet/plugin.py
@@ -8,8 +8,10 @@ import json
 class PluginTrajet:
 
     def response(self, args=None):
+        word = []
         if args is not None:
-            word= self.parseWord(args.group(1))
+            word.append(self.parseWord(args.group(1)))
+            word.append(self.parseWord(args.group(2)))
             return self.query(word)
         return "Je ne comprend pas la demande."
 
@@ -24,28 +26,29 @@ class PluginTrajet:
 
     def getPublicTransportBest(self, word):
         # get local position
-        g = geocoder.ip('me')
-        dlat, dlng = g.latlng
+        word[0] = word[0].replace('_', ' ')
+        geolocator = Nominatim(user_agent="Your_Name")
+        location = geolocator.geocode(word[0])
+        dlat, dlng = location.latitude, location.longitude
 
         # get dist position
-        word = word.replace('_', ' ')
+        word[1] = word[1].replace('_', ' ')
         geolocator = Nominatim(user_agent="Your_Name")
-        location = geolocator.geocode(word)
+        location = geolocator.geocode(word[1])
         elat, elng = location.latitude, location.longitude
 
         trajet = []
-
         try :
             e = "https://api.navitia.io/v1/coverage/fr-idf/journeys?key=88ede902-31c0-497b-a589-dff13c603a58&from={0}%3B{1}&to={2}%3B{3}&".format(
                 dlng, dlat, elng, elat)
+            print(e)
             r = requests.get(e)
             r = r.json()
-
             t = r['journeys'][0]
             trajet = self.makeReadableTransport(t)
 
             return json.dumps(trajet)
-        except Exception as e:
+        except :
             return trajet
 
 
@@ -54,7 +57,9 @@ class PluginTrajet:
         try :
             for sct in transport['sections']:
                 section = {}
-                if  sct['type'] == "street_network":
+                if sct['type'] == "waiting":
+                    continue
+                elif  sct['type'] == "street_network":
                     section.update({'type' : 'walking'})
                     section.update({'path' : sct['path']})
                 elif sct['type'] == "public_transport"  :
@@ -65,8 +70,9 @@ class PluginTrajet:
                     section.update({'type' : 'tranfer'})
                 section.update({'from' : sct['from']['name']})
                 section.update({'to' : sct['to']['name']})
-
+                a = []
+                a.append(section)
                 etape.append(section)
         except Exception as e:
-            pass
+            print(e)
         return etape
